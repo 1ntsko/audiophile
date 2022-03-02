10d0fe37f3205861a081d280eca6641fd0cb23bd
1ntsko <giorgilasha3@gmail.com>
2022-03-02 17:38:20 +0400

    main pages done

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
-const x = document.getElementById('close');
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.mobile-nav');
+const x = document.getElementById("close");
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".mobile-nav");
 
-const dropdownBasket = document.querySelector('.basket__dropdown');
-const overlay = document.querySelector('.overlay');
+const dropdownBasket = document.querySelector(".basket__dropdown");
+const overlay = document.querySelector(".overlay");
 
-// const basket = document.getElementById('basket');
-const basket = document.querySelector('#basket');
+const basket = document.querySelector("#basket");
 
-x.addEventListener('click', () => {
-  nav.style.left = '-100%';
+x.addEventListener("click", () => {
+  nav.style.left = "-100%";
 });
 
-burger.addEventListener('click', () => {
-  nav.style.left = '0';
+burger.addEventListener("click", () => {
+  nav.style.left = "0";
 });
 
-basket.addEventListener('click', () => {
-  overlay.classList.toggle('hidden');
+basket.addEventListener("click", () => {
+  overlay.classList.toggle("hidden");
 });
-
-// basket.forEach(i => {
-//   console.log(i);
-//   i.addEventListener('click', () => {
-//     overlay.classList.toggle('hidden');
-//   });
-// });
